21f8ef4fb58627a73f29387146f5fd36e629cef0
Sujin Sheela <sujinmsn@gmail.com>
2024-10-01 20:34:15 +0530

    changes

--- script.ps1
+++ script.ps1
@@ -1,7 +1,6 @@
 # script.ps1
 # A simple PowerShell script that lists all directories in the current path
 
-$data = Get-Process | ConvertTo-Html 
+Get-Process 
 # Modify script.ps1 to write to a mapped directory
-$data | Out-File "/output/file.html" -Force
 
